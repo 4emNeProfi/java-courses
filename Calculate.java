06daf704641fe54c268d0f53140269b0a9fad48f
4emNeProfi <dzasokhov1995@mail.ru>
2016-06-04 00:56:17 +0400

    create Calculate

--- Calculate.java
+++ Calculate.java
@@ -2,8 +2,19 @@ public class Calculate {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Hello words");
+		System.out.println("Calculate...");
 		
+		double first = Double.valueOf(args[0]);
+		double second = Double.valueOf(args[1]);
+		
+		System.out.println("Sum = " + (first+second));
+		System.out.println("Difference = " + (first-second));
+		System.out.println("Multiplication = " + (first*second));
+		System.out.println("Power = " + (Math.pow(first, second)));
+		if (second!=0){			
+			System.out.println("Private = " + (first/second));			
+		}else{
+			System.out.println("At zero divided cannot be !!");			
+		}
 	}
-
 }
